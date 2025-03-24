81502e064ddb47503487361a1165214657d713e2
erjondemaku <demakuerjon@gmail.com>
2025-03-24 22:38:32 +0100

    Bugfix: Mobile menu auto-close & smooth scrolling

--- files/assets/js/plugins/mobilemenu.js
+++ files/assets/js/plugins/mobilemenu.js
@@ -1,8 +1,7 @@
 ;(function($) {
-
   $(document).ready(function() {
 
-   //mobile-wrap
+    // Mobile wrap
     const mobile_nav_open = $('.mobile-nav-icon');
     const mobile_sidebar = $('.mobile-sidebar');
     const mobile_nav_close = $('.menu-close');
@@ -15,72 +14,64 @@
       mobile_sidebar.removeClass('mobile-menu-active');
     });
 
+    // Hide menu when clicking any menu link
+    $('.mobile-nav a').on('click', function(){
+      mobile_sidebar.removeClass('mobile-menu-active');
+    });
 
+    // Mobile menus
+    $('.mobile-nav a').each(function(){
+      var href = $(this).attr('href');
+      if(href === '#'){
+        $(this).addClass('hash-nav');
+      } else {
+        $(this).removeClass('hash-nav');
+      }
+    });
 
-  //mobile-menus
-  $('.mobile-nav a').each(function(){
-    var href = $(this).attr('href');
-    if(href = '#'){
-      $(this).addClass('hash-nav')
-    }else {
-      $(this).removeClass('hash-nav')
-    }
-  });
+    // Mobile menus markup
+    $.fn.menumarker = function(options){
+      var mobile_menu = $(this),
+      settings = $.extend({
+        format: "dropdown",
+        sticky: false
+      }, options);
 
-
-  //mobile-menus-markup
-  $.fn.menumarker = function(options){
-    mobile_menu = $(this),
-    settings = $.extend({
-      format: "dropdown",
-      sticky: false
-    }, options);
-
-
-    return this.each(function(){
-      mobile_menu.find('li ul').parent().addClass('has-sub');
-      var multiTg = function(){
-        mobile_menu.find('.hash-nav').parent().addClass('hash-has-sub');
-        mobile_menu.find(".has-sub").prepend('<span class="submenu-button"><em></em></span>');
-        mobile_menu.find('.submenu-button').on('click', function(){
-          $(this).toggleClass('submenu-opened');
+      return this.each(function(){
+        mobile_menu.find('li ul').parent().addClass('has-sub');
+        var multiTg = function(){
+          mobile_menu.find('.hash-nav').parent().addClass('hash-has-sub');
+          mobile_menu.find(".has-sub").prepend('<span class="submenu-button"><em></em></span>');
+          mobile_menu.find('.submenu-button').on('click', function(){
+            $(this).toggleClass('submenu-opened');
             if ($(this).siblings('ul').hasClass('open-sub')) {
-                $(this).siblings('ul').removeClass('open-sub').hide('fadeIn');
-                $(this).siblings('ul').hide('fadeIn');
+              $(this).siblings('ul').removeClass('open-sub').hide();
             } else {
-                $(this).siblings('ul').addClass('open-sub').hide('fadeIn');
-                $(this).siblings('ul').slideToggle().show('fadeIn');
+              $(this).siblings('ul').addClass('open-sub').slideToggle();
             }
-        });
-      };
+          });
+        };
 
-      if (settings.format === 'multitoggle') multiTg();
-      else mobile_menu.addClass('dropdown');
-      if (settings.sticky === true) mobile_menu.css('position', 'fixed');
-      var resizeFix = function () {
+        if (settings.format === 'multitoggle') multiTg();
+        else mobile_menu.addClass('dropdown');
+
+        if (settings.sticky === true) mobile_menu.css('position', 'fixed');
+
+        var resizeFix = function () {
           if ($(window).width() > 991) {
-              mobile_menu.find('ul').show('fadeIn');
-              mobile_menu.find('ul.sub-menu').hide('fadeIn');
+            mobile_menu.find('ul').show();
+            mobile_menu.find('ul.sub-menu').hide();
           }
-      };
-      resizeFix();
-      return $(window).on('resize', resizeFix);
+        };
 
-    })
-
-  }
-
+        resizeFix();
+        return $(window).on('resize', resizeFix);
+      });
+    };
 
     $('.mobile-nav').menumarker({
       format: "multitoggle"
     });
-   
 
   });
-
-
-
-
-
 })(jQuery);
-
